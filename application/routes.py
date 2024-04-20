e5162fea6ba5c62721b4c1621bf06e6c7faadffb
Cheikh10 <cheikhibrambengue90@gmail.com>
2024-04-20 15:06:44 +0000

    first commit

--- application/routes.py
+++ application/routes.py
@@ -8,29 +8,29 @@ from flask import render_template, flash , request
 from .forms import Classe
 from .forms import Etudiant
 
-@app.route("/")
-def index():
-    return render_template("view_classes.html", title = "Layout page")
+# @app.route("/")
+# def index():
+#     return render_template("view_classes.html", title = "Layout page")
 
-@app.route("/add_class", methods=["POST", "GET"])
-def add_class():
-    if request.method == "POST":
-       form = Classe(request.form)
-       classe_name = form.name.data
-       completed = form.completed.data
+# @app.route("/add_class", methods=["POST", "GET"])
+# def add_class():
+#     if request.method == "POST":
+#        form = Classe(request.form)
+#        classe_name = form.name.data
+#        completed = form.completed.data
        
-       db.etudiante_flask.insert_one({
-           "name": classe_name,    
-       })
-       flash("Etudiant successfully added", "success")
-       return redirect("/")
-    else:
-        form = Classe()
-    return render_template("add_class.html", form = form)
+#        db.etudiante_flask.insert_one({
+#            "name": classe_name,    
+#        })
+#        flash("Etudiant successfully added", "success")
+#        return redirect("/")
+#     else:
+#         form = Classe()
+#     return render_template("add_class.html", form = form)
 
-@app.route("/")
-def index():
-    return render_template("view_etudiant.html", title = "Layout page")
+# @app.route("/")
+# def index():
+#     return render_template("view_etudiant.html", title = "Layout page")
 
 @app.route("/add_etudiant", methods=["POST", "GET"])
 def add_etudiant():
